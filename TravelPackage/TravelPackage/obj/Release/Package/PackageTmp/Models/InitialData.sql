f42b154a72709f5762d7f1ecd2213a8b597e7457
Abel S. Salvatierra <assalvatierra@gmail.com>
2017-08-15 17:41:52 +0800

    just.merged

--- TravelPackage/TravelPackage/obj/Release/Package/PackageTmp/Models/InitialData.sql
+++ TravelPackage/TravelPackage/obj/Release/Package/PackageTmp/Models/InitialData.sql
@@ -16,10 +16,12 @@ Insert into [tpProducts]([Name],[ShortRemarks],[PageView],[PgFeatureImg], [Sort]
 ('Country Side Tour','7 - 9 hrs tour across the island of Bohol. Passing thru is cultural destinations and fascinating views of the main island','BoholPackages/CountrySide','/Images/Destinations/Bohol/CountrySideTour.png',1,2),
 ('Panglao Tour','8hrs island tour to the treasured beaches of Panglao.','BoholPackages/Panglao','/Images/Destinations/Bohol/PanglaoTour.png',2,2),
 ('3D2N Vacation','3Days 2 Nights Vacation Package. staying at the heart of the most visited beaches of Panglao.','BoholPackages/3d2n_Panglao_Alona42','/Images/Destinations/Bohol/3d2nBoholVacation.png',2,2),
-('Manila-Bohol','Economy Airticket Manila - Tagbilaran','ProductDefaultView2','/Images/Destinations/Bohol/PlanePAL.jpg',3,2),
-('J&R Residence','Hotel/Accommodation','ProductDefaultView2','/Images/Destinations/Bohol/hotel.JRresidence.jpg',4,2),
-('Blue Star Dive & Resort','Hotel/Accommodation','ProductDefaultView2','/Images/Destinations/Bohol/hotel.blue-star-dive-resort.jpg',5,2),
-('The Peacock Garden','Hotel/Accommodation','ProductDefaultView2','/Images/Destinations/Bohol/hotel.ThePeacockGarden.jpg',6,2);
+('Domestic Airticket Manila - Bohol','Economy Airticket Manila - Tagbilaran','BoholPackages/AirTicket','/Images/Destinations/Bohol/Airlines101.png',3,2),
+('Bohol Bee Farm - Panglao, Bohol','Resort & Restaurant','ProductDefaultView','/Images/Destinations/Bohol/accom_boholbeefarm_04.jpg',8,2),
+('Alona Tropical Beach Resort','Resort and Accommodation','ProductDefaultView','/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-10.jpg',5,2),
+('South Palms Resort Panglao','Resort and Accommodation','ProductDefaultView','/Images/Destinations/Bohol/accom_southpalmresort_08.jpg',6,2),
+('Dumaluan Beach Resort - Panglao','Resort and Accommodation','ProductDefaultView','/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-09.jpg',7,2),
+('Bohol Beach Club - Panglao, Bohol','Resort and Accommodation','ProductDefaultView','/Images/Destinations/Bohol/accom_bohol_beachclub_02.jpg',4,2);
 
 Insert into [tpProductImages]([tpProductsId], [ImgPath],[Desc],[AltName],[Sort]) values
 -- Bohol -CountrySide tour --
@@ -44,13 +46,110 @@ Insert into [tpProductImages]([tpProductsId], [ImgPath],[Desc],[AltName],[Sort])
 (5,'/Images/Destinations/Bohol/holiday_bohol_3d2n_02.jpg','Hinagdanan Cave','Hinagdanan Cave',2),
 (5,'/Images/Destinations/Bohol/holiday_bohol_3d2n_03.jpg','Panglao Church','Panglao Church',3),
 (5,'/Images/Destinations/Bohol/holiday_bohol_3d2n_04.jpg','Bee Farm','Bee Farm',4),
-(5,'/Images/Destinations/Bohol/holiday_bohol_3d2n_05.jpg','Beach Resort','Beach Resort',5);
+(5,'/Images/Destinations/Bohol/holiday_bohol_3d2n_05.jpg','Beach Resort','Beach Resort',5),
+-- Bohol -Bee Farm resort and Restaurant--
+(7,'/Images/Destinations/Bohol/accom_boholbeefarm_01.jpg','Bee Farm Restaurant','Bee Farm Restaurant',1),
+(7,'/Images/Destinations/Bohol/accom_boholbeefarm_02.jpg','Ocean view','Ocean view',2),
+(7,'/Images/Destinations/Bohol/accom_boholbeefarm_03.jpg','at night','Bee farm at night',3),
+(7,'/Images/Destinations/Bohol/accom_boholbeefarm_04.jpg','Pool View','Bee farm Pool View',4),
+(7,'/Images/Destinations/Bohol/accom_boholbeefarm_05.jpg','Pool','Bee Farm Pool',5),
+(7,'/Images/Destinations/Bohol/accom_boholbeefarm_06.jpg','Garden','Bee Farm Garden',6),
+-- Alona Tropical Beach Resort--
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-01.jpg','','Alona Tropical Beach Resort',1),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-02.jpg','','Alona Tropical Beach Resort',2),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-03.jpg','','Alona Tropical Beach Resort',3),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-04.jpg','','Alona Tropical Beach Resort',4),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-05.jpg','','Alona Tropical Beach Resort',5),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-06.jpg','','Alona Tropical Beach Resort',6),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-07.jpg','','Alona Tropical Beach Resort',7),
+--(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-08.jpg','','Alona Tropical Beach Resort',8),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-09.jpg','','Alona Tropical Beach Resort',9),
+--(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-10.jpg','','Alona Tropical Beach Resort',10),
+--(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-11.jpg','','Alona Tropical Beach Resort',11),
+(8,'/Images/Destinations/Bohol/accom_alona-tropical-beach-resort-12.jpg','','Alona Tropical Beach Resort',12),
+-- South Palms Beach Resort--
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_01.jpg','','South Palms Beach Resort',1),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_02.jpg','','South Palms Beach Resort',2),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_03.jpg','','South Palms Beach Resort',3),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_04.jpg','','South Palms Beach Resort',4),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_05.jpg','','South Palms Beach Resort',5),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_06.jpg','','South Palms Beach Resort',6),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_07.jpg','','South Palms Beach Resort',7),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_08.jpg','','South Palms Beach Resort',8),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_09.jpg','','South Palms Beach Resort',9),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_10.jpg','','South Palms Beach Resort',10),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_11.jpg','','South Palms Beach Resort',11),
+(9,'/Images/Destinations/Bohol/accom_southpalmresort_12.jpg','','South Palms Beach Resort',12),
+-- Dumaluan Beach Resort--
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-01.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-02.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-03.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-04.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-05.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-06.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-07.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-08.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-09.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-10.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-11.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-12.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-13.jpg','','Dumaluan beach resort',1),
+(10,'/Images/Destinations/Bohol/accom_Dumaluan_beach-resort-14.jpg','','Dumaluan beach resort',1),
+-- Bohol Beach Club--
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_01.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_02.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_03.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_04.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_05.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_06.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_07.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_08.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_09.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_10.jpg','','Bohol Beacg Club',1),
+(11,'/Images/Destinations/Bohol/accom_bohol_beachclub_11.jpg','','Bohol Beacg Club',1);
+
+
 
 Insert into tpProdCats([tpCategoryId],[tpProductsId]) values
 (1,1),(1,2),(1,3),(1,4),(1,5), -- Featured - Tour Packages 
-(2,6),(2,7),(2,8),(2,9); -- add on products
+(2,6),(2,7),(2,8),(2,9),(2,10),(2,11); -- bohol add on products
 
 Insert into tpProdRates([tpProductsId],[tpUomId],[GroupOf],[Rate],[Remarks],[Sort]) values
 (3,2,1,4250,'',1),(3,2,2,2600,'',1),(3,2,3,2100,'',1),(3,2,4,1950,'',1),(3,2,5,1800,'',1),(3,2,6,1700,'',1),(3,2,7,1600,'',1),(3,2,8,1550,'',1),(3,2,9,1450,'',1),(3,2,10,1350,'',1);
 
+Insert into tpProductDescs([tpProductsId],[tpDescH1],[tpDescH2], [tpDesc],[Sort]) values
+(7,'Garden Rooms - Php3,000','(12 units, 25sqm, 3pax)','Inspired by fruits and vegetables ingredients with farm.',1),
+(7,'Deluxe Rooms - Php4,000','(8 units, 30sqm, 3pax)','These beehive rooms are our garden view rooms. Have veranda and lounge.',2),
+(7,'Family Rooms - Php5,000','(6 units)','2 queen size beds, 2 toilet and bath',3),
+(7,'Ocean View - Php6,000','(8 units)','Spacious and veranda and lounge with excellent view of the beach. 1 king size beds and 1 single bed',4),
+(7,'','','-------------------------------------',5),
+(7,'Extra Person - Php800/night','','',6),
+(7,'Amenities','','- Farm Tour (Organic farming, Craft & Livelihood, Raffia Weaving, Bee Colony)',7),
+(7,'','','- Dive Center',8),
+(7,'','','- Massage SPA and Wellness',9),
+(7,'','','- Function Halls (Wedding, Birthday, Special Events)',10),
+-- Alona rooms --
+(8,'Double Deluxe','Php 3,360 per room/night','Tax inclusive',1),
+(8,'','Php 4,015 per room/night','With BreakFast, Tax inclusive',2),
+(8,'','Php 4,730 per room/night','Half Board, Tax inclusive',3),
+(8,'','Php 5,445 per room/night','Full Board, Tax inclusive',4),
+(8,'Suite Beach Front','Php 3,360 per room/night','Tax inclusive',5),
+(8,'','Php 6,815 per room/night','With BreakFast, Tax inclusive',6),
+(8,'','Php 7,530 per room/night','Half Board, Tax inclusive',7),
+(8,'','Php 8,245 per room/night','Full Board, Tax inclusive',8),
+(8,'Suite Pool View','Php 6,750 per room/night','Tax inclusive',9),
+(8,'','Php 7,450 per room/night','With BreakFast, Tax inclusive',10),
+(8,'','Php 8,130 per room/night','Half Board, Tax inclusive',11),
+(8,'','Php 8,840 per room/night','Full Board, Tax inclusive',12),
 
+-- South Palms  --
+(9,'Deluxe - Double or Twin','Php 9,125 per night Pool View','With Breakfast for 2',1),
+(9,'','Php 11,050 per night Beach Front','With Breakfast for 2',2),
+(9,'Premium - Double or Twin','Php 18,650 per night Beach Front','With Breakfast for 2',3),
+(9,'Panglao Villa','Php 39,050 per night','With Breakfast for 2',3),
+
+-- Dumaluan  --
+(10,'Room Rates','No Available as of this time','Contact our travel consultant for reservation',1),
+
+-- Bohol Beach Club --
+(11,'Room Rates','No Available as of this time','Contact our travel consultant for reservation',1);

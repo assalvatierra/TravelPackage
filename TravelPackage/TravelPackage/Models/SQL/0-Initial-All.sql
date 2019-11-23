bcac535a9321e658eb85a5878eb3ec1bb444bbcd
Jahdiel Villosa <jahdielsvillosa@gmail.com>
2019-11-22 18:21:20 -0800

    Sql update for cebu

--- TravelPackage/TravelPackage/Models/SQL/0-Initial-All.sql
+++ TravelPackage/TravelPackage/Models/SQL/0-Initial-All.sql
@@ -255,12 +255,6 @@ Insert into tpBacklinks([LinkType],[LinkUrl],[Description],[LinkExpiry],[Status]
 ('PRODUCT','http://realwheelsdavao.com/','Real Wheels Davao  Rent a car in Davao','2020-01-01','ACT');
 
 
---Readme
---Adding new products with cshtml views
--- add in the tpProducts, add description and images
--- add cshtml under views/(Destination)Packages, add content / ProductDefaultView - set desciption intpProductDescs
--- add add images in sql
--- add in tpProdCats to either main view or sub view
 
 
 Insert into [tpProducts]([Name],[ShortRemarks],[PageView],[PgFeatureImg], [Sort], [tpAreasId]) values 
@@ -402,11 +396,7 @@ Insert into tpKeywords([Keyword],[tpProductsId]) values
 ('Palawan','29'),('Tour','29'),('Travel','29'),('Vacation','29'),('Package','29'),('Puerto Princesa','29'),
 ('Palawan','30'),('Tour','30'),('Travel','30'),('Vacation','30'),('Package','30'),('Puerto Princesa','30'),
 ('Palawan','31'),('Tour','31'),('Travel','31'),('Vacation','31'),('Package','31'),('Puerto Princesa','31')
-;
-
-
-
---Readme
+;--Readme
 --Adding new products with cshtml views
 -- add in the tpProducts, add description and images
 -- add cshtml under views/(Destination)Packages, add content / ProductDefaultView - set desciption intpProductDescs
@@ -445,7 +435,6 @@ Insert into [tpProductImages]([tpProductsId], [ImgPath],[Desc],[AltName],[Sort])
 -- oslob dolphin watching                
 (34,'/Images/Destinations/Cebu/Oslob/whale-shark-oslob.jpg','Whale Shark','Whale Shark',1),
 (34,'/Images/Destinations/Cebu/Oslob/whale-shark.jpg','Whale Shark','Whale Shark',2)
-
 ;
 
 
@@ -458,7 +447,9 @@ Insert into tpKeywords([Keyword],[tpProductsId]) values
 ('Cebu','29'),('Tour','29'),('Travel','29'),('Vacation','29'),('Package','29'),
 ('Cebu','30'),('Tour','30'),('Travel','30'),('Vacation','30'),('Package','30'),
 ('Cebu','31'),('Tour','31'),('Travel','31'),('Vacation','31'),('Package','31')
-;--Readme
+;
+
+--Readme
 --Adding new products with cshtml views
 -- add in the tpProducts, add description and images
 -- add cshtml under views/(Destination)Packages, add content / ProductDefaultView - set desciption intpProductDescs
@@ -533,7 +524,9 @@ Insert into tpKeywords([Keyword],[tpProductsId]) values
 ('Thailand','36'),('Tour','36'),('Travel','36'),('Vacation','36'),('Package','36'),('International','36'), --Thailand
 ('Korea','37')   ,('Tour','37'),('Travel','37'),('Vacation','37'),('Package','37'),('International','37'), --Korea
 ('Singapore','37'),('Tour','37'),('Travel','37'),('Vacation','37'),('Package','37'),('International','38') --Singapore
-;--Readme
+;
+
+--Readme
 -- Adding new products with cshtml views
 -- add in the tpProducts, add description and images
 -- add cshtml under views/(Destination)Packages, add content / ProductDefaultView - set desciption intpProductDescs
@@ -607,7 +600,8 @@ Insert into tpKeywords([Keyword],[tpProductsId]) values
 ('Ilocos','41'),('Tour','41'),('Travel','41'),('Vacation','41'),('Package','41'),
 ('Ilocos','42'),('Tour','42'),('Travel','42'),('Vacation','42'),('Package','42'),
 ('Ilocos','43'),('Tour','43'),('Travel','43'),('Vacation','43'),('Package','43')
-;--Readme
+;
+--Readme
 -- Adding new products with cshtml views
 -- add in the tpProducts, add description and images
 -- add cshtml under views/(Destination)Packages, add content / ProductDefaultView - set desciption intpProductDescs
@@ -644,4 +638,91 @@ Insert into tpProdCats([tpCategoryId],[tpProductsId]) values
 
 Insert into tpKeywords([Keyword],[tpProductsId]) values
 ('Ilocos','44'),('Tour','44'),('Travel','44'),('Vacation','44'),('Package','44')
+;
+
+--Readme
+--Adding new products with cshtml views
+-- add in the tpProducts, add description and images
+-- add cshtml under views/(Destination)Packages, add content / ProductDefaultView - set desciption intpProductDescs
+-- add add images in sql
+-- add in tpProdCats to either main view or sub view
+
+
+Insert into [tpProducts]([Name],[ShortRemarks],[PageView],[PgFeatureImg], [Sort], [tpAreasId]) values 
+
+--45-50,
+('Cebu Kawasan and Moalboal Tour','Kawasan and Moalboal Tour','CebuPackages/CebuKawasanMoalboal','/Images/Destinations/Cebu/Kawasan-Moalboal/Cebu-Kawasan-Moalboal-Tour.png',4,3),
+('Cebu Canyoneering and Moalboal Tour','Cebu Canyoneering and Moalboal Tour','CebuPackages/CanyoneeringMoalboal','/Images/Destinations/Cebu/Canyoneering-Moalboal/Canyoneering-Moalboal.png',5,3),
+('Simala and Cebu City Tour Package','Simala Cebu Tour Package','CebuPackages/SimalaCebuTour','/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Simala-Cebu-Tour-Package.png',6,3),
+('Oslob Whaleshark Watching and Kawasan Tour','Oslob Whale Shark Watching and Kawasan Tour','CebuPackages/OslobWhalesharkKawasan','/Images/Destinations/Cebu/Whaleshark-Kawasan/Oslob-Whale-Kawasan.png',7,3),
+('Oslob Whaleshark Watching with Sumilon and Tumalog Tour','Oslob Whale Shark Watching w/ Sumilon & Tumalog Tour','CebuPackages/OslobWhalesharkSumilonTumalog','/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/Oslob-Whale-Sumilon-Tumalog.png',8,3),
+('Oslob Whaleshark Watching with Sumilon and Kawasan Tour','Oslob Whale Shark Watching w/ Sumilon & Kawasan Tour','CebuPackages/OslobWhalesharkSumilonKawasan','/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/Whaleshark-Sumilon-Kawasan.png',9,3),
+
+--51-52,
+
+('Simala with Oceanpark Tour Package','Simala & Cebu City w/ Oceanpark Tour','CebuPackages/SimalaCebuOceanparkTour','/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Simala-Cebu-Oceanpark.png',11,3),
+('Cebu with Oceanpark Tour Package','Cebu City w/ Oceanpark Tour','CebuPackages/CebuOceanparkTour','/Images/Destinations/Cebu/Cebu-Oceanpark/Cebu-City-Oceanpark.png',12,3)
+;
+
+Insert into [tpProductImages]([tpProductsId], [ImgPath],[Desc],[AltName],[Sort]) values
+
+-- Kawasan and Moalboal Tour             
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Pescador-Island.png','Pescador Island','Pescador Island',1),
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Turtle-Point.png','Turtle Point','Turtle Point',2),
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Sardine-Run.png','Sardine-Run','Sardine-Run',3),
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Kawasan-Falls.png','Kawasan Falls','Kawasan Falls',4),
+
+-- Cebu Canyoneering and Moalboal Tour             
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Pescador-Island.png','Pescador Island','Pescador Island',1),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Turtle-Point.png','Turtle Point','Turtle Point',2),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Sardine-Run.png','Sardine-Run','Sardine-Run',3),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Kawasan-Falls.png','Kawasan Falls','Kawasan Falls',4),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Canyoneering.png','Canyoneering','Canyoneering',5),
+
+-- Simala and Cebu City Tour             
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Simala-Church.jpg','Simala Church','Simala Church',1),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Sirao-Flower-Garden.png','Sirao Flower Garden','Sirao Flower Garden',2),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Sto-Nino-Cathedral.jpg','Sto Nino Cathedral','Sto Nino Cathedral',3),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Taoist-Temple.jpg','Kawasan Falls','Kawasan Falls',4),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Temple-of-Leah.png','Canyoneering','Canyoneering',5),
+
+-- Oslob Whaleshark watching and kawasan tour  
+(48,'/Images/Destinations/Cebu/Whaleshark-Kawasan/whale-shark.jpg','Whaleshark Watching','Whaleshark Watching',1),
+(48,'/Images/Destinations/Cebu/Whaleshark-Kawasan/Kawasan-Falls.png','Kawasan Falls','Kawasan Falls',2),
+
+-- Oslob Whaleshark watching with Sumilon and Tumalog
+(49,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/whale-shark.jpg','Whaleshark Watching','Whaleshark Watching',1),
+(49,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/Sumilon-Island.png','Sumilon Island','Sumilon Island',2),
+(49,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/Tumalog-Falls.png','Tumalog-Falls','Tumalog-Falls',3),
+
+-- Oslob Whaleshark watching with Sumilon and Kawasan
+(50,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/whale-shark.jpg','Whaleshark Watching','Whaleshark Watching',1),
+(50,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/Sumilon-Island.png','Sumilon Island','Sumilon Island',2),
+(50,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/Tumalog-Falls.png','Tumalog-Falls','Tumalog-Falls',3),
+
+-- Simala with Oceanpark Tour Package
+(51,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Simala-Church.jpg','Simala Shrine','Simala-Shrine',1),
+(51,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Cebu-Oceanpark-2.png','Cebu Oceanpark','Cebu Oceanpark',2),
+
+-- Cebu with Oceanpark Tour Package
+(52,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Simala-Church.jpg','Simala Shrine','Simala-Shrine',1),
+(52,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Cebu-Oceanpark-2.png','Cebu Oceanpark','Cebu Oceanpark',2)
+;
+
+
+Insert into tpProdCats([tpCategoryId],[tpProductsId]) values
+(1,45),(1,46),(1,47),(1,48),(1,49)  ,    -- Cebu
+(1,50),(1,51),(1,52) 
+; 
+
+Insert into tpKeywords([Keyword],[tpProductsId]) values
+('Cebu','45'),('Tour','45'),('Travel','45'),('Vacation','45'),('Package','45'),
+('Cebu','46'),('Tour','46'),('Travel','46'),('Vacation','46'),('Package','46'),
+('Cebu','47'),('Tour','47'),('Travel','47'),('Vacation','47'),('Package','47'),
+('Cebu','48'),('Tour','48'),('Travel','48'),('Vacation','48'),('Package','48'),
+('Cebu','49'),('Tour','49'),('Travel','49'),('Vacation','49'),('Package','49'),
+('Cebu','50'),('Tour','50'),('Travel','50'),('Vacation','50'),('Package','50'),
+('Cebu','51'),('Tour','51'),('Travel','51'),('Vacation','51'),('Package','51'),
+('Cebu','52'),('Tour','52'),('Travel','52'),('Vacation','52'),('Package','52'),
+('Cebu','53'),('Tour','53'),('Travel','53'),('Vacation','53'),('Package','53')
 ;
--- TravelPackage/TravelPackage/Models/SQL/7-Cebu-new.sql
+++ TravelPackage/TravelPackage/Models/SQL/7-Cebu-new.sql
@@ -22,18 +22,58 @@ Insert into [tpProducts]([Name],[ShortRemarks],[PageView],[PgFeatureImg], [Sort]
 ('Cebu with Oceanpark Tour Package','Cebu City w/ Oceanpark Tour','CebuPackages/CebuOceanparkTour','/Images/Destinations/Cebu/Cebu-Oceanpark/Cebu-City-Oceanpark.png',12,3)
 ;
 
+
 Insert into [tpProductImages]([tpProductsId], [ImgPath],[Desc],[AltName],[Sort]) values
 
 -- Kawasan and Moalboal Tour             
-(45,'/Images/Destinations/Cebu/Oslob/whale-shark-oslob.jpg','Whale Shark','Whale Shark',1),
-(45,'/Images/Destinations/Cebu/Oslob/whale-shark.jpg','Whale Shark','Whale Shark',2)
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Pescador-Island.png','Pescador Island','Pescador Island',1),
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Turtle-Point.png','Turtle Point','Turtle Point',2),
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Sardine-Run.png','Sardine-Run','Sardine-Run',3),
+(45,'/Images/Destinations/Cebu/Kawasan-Moalboal/Kawasan-Falls.png','Kawasan Falls','Kawasan Falls',4),
+
+-- Cebu Canyoneering and Moalboal Tour             
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Pescador-Island.png','Pescador Island','Pescador Island',1),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Turtle-Point.png','Turtle Point','Turtle Point',2),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Sardine-Run.png','Sardine-Run','Sardine-Run',3),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Kawasan-Falls.png','Kawasan Falls','Kawasan Falls',4),
+(46,'/Images/Destinations/Cebu/Canyoneering-Moalboal/Canyoneering.png','Canyoneering','Canyoneering',5),
+
+-- Simala and Cebu City Tour             
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Simala-Church.jpg','Simala Church','Simala Church',1),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Sirao-Flower-Garden.png','Sirao Flower Garden','Sirao Flower Garden',2),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Sto-Nino-Cathedral.jpg','Sto Nino Cathedral','Sto Nino Cathedral',3),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Taoist-Temple.jpg','Kawasan Falls','Kawasan Falls',4),
+(47,'/Images/Destinations/Cebu/Simala-Cebu-Tour-Package/Temple-of-Leah.png','Canyoneering','Canyoneering',5),
+
+-- Oslob Whaleshark watching and kawasan tour  
+(48,'/Images/Destinations/Cebu/Whaleshark-Kawasan/whale-shark.jpg','Whaleshark Watching','Whaleshark Watching',1),
+(48,'/Images/Destinations/Cebu/Whaleshark-Kawasan/Kawasan-Falls.png','Kawasan Falls','Kawasan Falls',2),
+
+-- Oslob Whaleshark watching with Sumilon and Tumalog
+(49,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/whale-shark.jpg','Whaleshark Watching','Whaleshark Watching',1),
+(49,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/Sumilon-Island.png','Sumilon Island','Sumilon Island',2),
+(49,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Tumalog/Tumalog-Falls.png','Tumalog-Falls','Tumalog-Falls',3),
+--Simala and Cebu Tour Package
+--duplicate
+-- Oslob Whaleshark watching with Sumilon and Kawasan
+(50,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/whale-shark.jpg','Whaleshark Watching','Whaleshark Watching',1),
+(50,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/Sumilon-Island.png','Sumilon Island','Sumilon Island',2),
+(50,'/Images/Destinations/Cebu/Whaleshark-Sumilon-Kawasan/Tumalog-Falls.png','Tumalog-Falls','Tumalog-Falls',3),
+
+-- Simala with Oceanpark Tour Package
+(52,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Simala-Church.jpg','Simala Shrine','Simala-Shrine',1),
+(52,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Cebu-Oceanpark-2.png','Cebu Oceanpark','Cebu Oceanpark',2),
+
+-- Cebu with Oceanpark Tour Package
+(53,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Simala-Church.jpg','Simala Shrine','Simala-Shrine',1),
+(53,'/Images/Destinations/Cebu/Simala-Cebu-Oceanpark/Cebu-Oceanpark-2.png','Cebu Oceanpark','Cebu Oceanpark',2)
 ;
+
 
 Insert into tpProdCats([tpCategoryId],[tpProductsId]) values
 (1,45),(1,46),(1,47),(1,48),(1,49)  ,    -- Cebu
-(1,50),(1,51),(1,52) 
+(1,50),(1,51),(1,52) ,(1,53) 
 ; 
-
 Insert into tpKeywords([Keyword],[tpProductsId]) values
 ('Cebu','45'),('Tour','45'),('Travel','45'),('Vacation','45'),('Package','45'),
 ('Cebu','46'),('Tour','46'),('Travel','46'),('Vacation','46'),('Package','46'),
